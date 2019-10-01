3a6c80091559dcbb431761e62ec9d6a0fef661f6
Sondre <sondrefr@gmail.com>
2019-10-01 18:57:52 +0200

    server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const morgan = require('morgan');
+
+app.use(morgan('dev'));
+
+app.get('/', function (request, response) {
+    response.send('Hello World');
+  });
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`); // Note the `` backticks is NOT the same as single quotes ''
+});
+
